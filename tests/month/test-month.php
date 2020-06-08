bdbe245ea87e5a0fa40675fb0204d95392b61da3
Nilambar Sharma <nilambar@outlook.com>
2020-06-08 15:29:41 +0545

    Adding tests

---
+++ tests/month/test-month.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Class MonthTest
+ *
+ * @package Ns_Nepali_Date
+ */
+
+/**
+ * Month test case.
+ */
+class MonthTest extends WP_UnitTestCase {
+
+	/**
+	 * Test nepali month Baishakh.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_month_baishakh() {
+		$month = ns_nepali_date_get_month_text( 1 );
+
+		$this->assertEquals( 'Baishakh', $month );
+	}
+
+	/**
+	 * Test english month Baishakh Devnagari.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_month_baishakh_devnagari() {
+		$month = ns_nepali_date_get_month_text( 1, 'np' );
+
+		$this->assertEquals( 'बैसाख', $month );
+	}
+
+	/**
+	 * Test nepali month Chaitra.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_month_chaitra() {
+		$month = ns_nepali_date_get_month_text( 12 );
+
+		$this->assertEquals( 'Chaitra', $month );
+	}
+
+	/**
+	 * Test nepali month Chaitra Devnagari.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_month_chaitra_devnagari() {
+		$month = ns_nepali_date_get_month_text( 12, 'np' );
+
+		$this->assertEquals( 'चैत्र', $month );
+	}
+}
---
+++ tests/number/test-number.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Class NumberTest
+ *
+ * @package Ns_Nepali_Date
+ */
+
+/**
+ * Number test case.
+ */
+class NumberTest extends WP_UnitTestCase {
+
+	/**
+	 * Test nepali number single digit.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_number_single_digit() {
+		$out = ns_nepali_date_get_nepali_number( 1 );
+
+		$this->assertEquals( '१', $out );
+	}
+
+	/**
+	 * Test nepali number double digit.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_number_double_digit() {
+		$out = ns_nepali_date_get_nepali_number( 25 );
+
+		$this->assertEquals( '२५', $out );
+	}
+
+	/**
+	 * Test nepali number single digit with padding 2.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_number_single_digit_with_padding_2() {
+		$out = ns_nepali_date_get_nepali_number( 1, true );
+
+		$this->assertEquals( '०१', $out );
+	}
+
+	/**
+	 * Test nepali number single digit with padding 4.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_nepali_number_single_digit_with_padding_4() {
+		$out = ns_nepali_date_get_nepali_number( 1, true, 4 );
+
+		$this->assertEquals( '०००१', $out );
+	}
+
+
+}
