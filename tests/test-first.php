4146faad1d756e5820e01fb69240d4f01cff2f90
Nilambar Sharma <nilambar@outlook.com>
2020-06-08 14:48:37 +0545

    Add test setting

---
+++ tests/test-first.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class FirstTest
+ *
+ * @package Ns_Nepali_Date
+ */
+
+/**
+ * Sample test case.
+ */
+class FirstTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Ns_Nepali_Date
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
