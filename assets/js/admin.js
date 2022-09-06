653443c8b96ddad421ee7ba5e70b0b2a11aec900
Nilambar Sharma <nilambar@outlook.com>
2022-09-06 15:24:14 +0545

    fix: sidebar

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,67 @@
-(function( $ ) {
-	'use strict';
+'use strict';
+
+( function( $ ) {
+	$.fn.blogPosts = function( options ) {
+		var settings = $.extend( {
+			api: '',
+			action: 'blog_posts',
+			loading_text: 'Loading',
+			list_type: 'ul',
+		}, options );
+
+		if ( '' === settings.api ) {
+			return this;
+		}
+
+		function generateList( data ) {
+			var output = '';
+
+			if ( 0 === data.length ) {
+				return output;
+			}
+
+			data.forEach( function( item ) {
+				output += '<li><a href="' + item.url + '" target="_blank">' + item.title + '</a></li>';
+			} );
+
+			return $( '<' + settings.list_type + '/>' ).append( output );
+		}
+
+		return this.each( function() {
+			if ( $(this).length < 1 ) {
+				return;
+			}
+
+			var $wrapper = $( this );
+
+			$.ajax( {
+				url: settings.api,
+				type: 'GET',
+				dataType: 'json',
+				data: { action: settings.action },
+				beforeSend: function() {
+					$wrapper.html( settings.loading_text );
+				},
+				complete: function( jqXHR ) {
+					var response = JSON.parse( jqXHR.responseText );
+
+					$wrapper.html( '' );
+
+					if ( true === response.success ) {
+						var listMarkup = generateList( response.data );
+						$wrapper.append( listMarkup );
+					}
+				},
+			} );
+		} );
+	};
 
 	$(document).ready(function() {
+		$( '.ns-blog-list' ).blogPosts( {
+			api: ajaxurl,
+			action: 'nsnd_get_posts',
+		} );
+
 		$('.format-list a').on('click',function(e){
 			e.preventDefault();
 			var $this = $(this);
@@ -15,6 +75,4 @@
 			$('.format-reference-content').fadeToggle();
 		});
 	});
-
-
 })( jQuery );
