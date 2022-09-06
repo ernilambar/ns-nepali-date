0b98b5ee14a427dd827213656ab3d00067568369
Nilambar Sharma <nilambar@outlook.com>
2022-09-06 15:00:24 +0545

    feat: setup

--- ns-nepali-date.php
+++ ns-nepali-date.php
@@ -3,7 +3,7 @@
  * Plugin Name: NS Nepali Date
  * Plugin URI: https://github.com/ernilambar/ns-nepali-date/
  * Description: Display post date in Nepali.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * Text Domain: ns-nepali-date
@@ -21,13 +21,21 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'NS_NEPALI_DATE_VERSION', '1.0.7' );
+define( 'NS_NEPALI_DATE_VERSION', '1.0.8' );
 define( 'NS_NEPALI_DATE_SLUG', 'ns-nepali-date' );
 define( 'NS_NEPALI_DATE_BASENAME', basename( dirname( __FILE__ ) ) );
 define( 'NS_NEPALI_DATE_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
 define( 'NS_NEPALI_DATE_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 
-require_once NS_NEPALI_DATE_DIR . '/vendor/autoload.php';
+define( 'OPTIONER_DIR', NS_NEPALI_DATE_DIR . '/vendor/ernilambar/optioner' );
+define( 'OPTIONER_URL', NS_NEPALI_DATE_URL . '/vendor/ernilambar/optioner' );
+
+
+if ( file_exists( NS_NEPALI_DATE_DIR . '/vendor/autoload.php' ) ) {
+	require_once NS_NEPALI_DATE_DIR . '/vendor/autoload.php';
+	require_once NS_NEPALI_DATE_DIR . '/vendor/ernilambar/optioner/optioner.php';
+}
+
 require_once NS_NEPALI_DATE_DIR . '/inc/classes/class-ns-nepali-date.php';
 require_once NS_NEPALI_DATE_DIR . '/inc/classes/class-ns-nepali-date-admin.php';
 require_once NS_NEPALI_DATE_DIR . '/inc/helpers/helpers.php';
