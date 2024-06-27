ff2a47d142897a05a6bcc3db3a051efea1934038
Nilambar Sharma <nilambar@outlook.com>
2024-06-27 22:31:10 +0545

    Add PostCSS

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,23 @@
-import "dotenv/config";
-import { v4wp } from "@kucrut/vite-for-wp";
-import VitePluginBrowserSync from "vite-plugin-browser-sync";
-import postcssPresetEnv from "postcss-preset-env";
-import postcssNested from "postcss-nested";
+import 'dotenv/config';
+import { v4wp } from '@kucrut/vite-for-wp';
+import VitePluginBrowserSync from 'vite-plugin-browser-sync';
 
 export default {
 	build: {
-		target: "es2015",
-	},
-	css: {
-		postcss: {
-			plugins: [postcssPresetEnv, postcssNested],
-		},
+		target: 'es2015',
 	},
 	plugins: [
-		v4wp({
-			input: ["src/admin.js"],
-			outDir: "build",
-		}),
-		VitePluginBrowserSync({
+		v4wp( {
+			input: [ 'src/admin.js' ],
+			outDir: 'build',
+		} ),
+		VitePluginBrowserSync( {
 			dev: {
 				bs: {
 					proxy: process.env.DEV_SERVER_URL,
-					open: "yes" === process.env.BROWSERSYNC_OPEN ? true : false,
+					open: 'yes' === process.env.BROWSERSYNC_OPEN ? true : false,
 				},
 			},
-		}),
+		} ),
 	],
 };
